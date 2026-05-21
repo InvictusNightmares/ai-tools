4f6e9ad6d3cc17363fad709b3ea935548c6c648c
Invictus <invictusnightmares@gmail.com>
2026-05-21 18:30:31 +0800

    optimize code

--- script/code-agent/install.js
+++ script/code-agent/install.js
@@ -274,7 +274,7 @@ function homeDir() {
 }
 
 function configDir() {
-  if (process.platform === 'win32' || process.platform === 'darwin') {
+  if (process.platform === 'win32') {
     return path.join(homeDir(), '.config');
   }
   return process.env.XDG_CONFIG_HOME || path.join(homeDir(), '.config');
